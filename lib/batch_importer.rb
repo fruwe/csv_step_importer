8f8d22c0d320ed32b632112ee6d0eee1fc9aa5c2
Christian-Manuel Butzke <chris@fruwe.com>
2018-03-19 15:46:11 +0900

    move classes

--- lib/batch_importer.rb
+++ lib/batch_importer.rb
@@ -1,3 +1,5 @@
+require 'batch_importer/version'
+
 require 'batch_importer/base'
 require 'batch_importer/node'
 require 'batch_importer/row'
@@ -8,7 +10,6 @@ require 'batch_importer/model/model'
 require 'batch_importer/model/reflector'
 require 'batch_importer/model/importer'
 require 'batch_importer/model/importable_model'
-require 'batch_importer/version'
 
 module BatchImporter
   # Your code goes here...
