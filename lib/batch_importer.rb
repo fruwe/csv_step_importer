75bdb7fb39b24fcbe844fc996b2d88a8d670daf1
Christian-Manuel Butzke <chris@fruwe.com>
2018-03-19 15:36:20 +0900

    add require

--- lib/batch_importer.rb
+++ lib/batch_importer.rb
@@ -1,4 +1,14 @@
-require "batch_importer/version"
+require 'lib/batch_importer/base'
+require 'lib/batch_importer/node'
+require 'lib/batch_importer/row'
+require 'lib/batch_importer/file'
+require 'lib/batch_importer/chunk'
+require 'lib/batch_importer/model/dao'
+require 'lib/batch_importer/model/model'
+require 'lib/batch_importer/model/reflector'
+require 'lib/batch_importer/model/importer'
+require 'lib/batch_importer/model/importable_model'
+require 'lib/batch_importer/version'
 
 module BatchImporter
   # Your code goes here...
