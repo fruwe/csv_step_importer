f66a6ea8d93d23fcba96c30f6255cb5a6541faf2
Christian-Manuel Butzke <chris@fruwe.com>
2018-04-03 20:35:06 +0900

    Bump to 0.5.5

--- lib/csv_step_importer/node.rb
+++ lib/csv_step_importer/node.rb
@@ -10,7 +10,7 @@ module CSVStepImporter
     delegate :cache, to: :root
     delegate :root, to: :parent
 
-    after_validation :validate_children
+    validate :validate_children
 
     def initialize(parent: nil, children: [], env: nil)
       super()
